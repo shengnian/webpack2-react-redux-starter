3aa7991d92a3f23e4fbb0ec7b49acc5df67090ff
C <c@shengnian.org>
2017-01-02 16:05:10 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Shengnian UI
+A Component Library for React
+
+## Table of Contents
+1. [Features](#features)
+1. [Requirements](#requirements)
+1. [Getting Started](#getting-started)
+
+## Features
+* [react](https://github.com/facebook/react)
+* [redux](https://github.com/rackt/redux)
+* [react-router](https://github.com/rackt/react-router)
+* [webpack 2](https://github.com/webpack/webpack)
+* [Migrating from v1 to v2](https://webpack.js.org/guides/migrating/)
+* [babel](https://github.com/babel/babel)
+* [express](https://github.com/expressjs/express)
+* [karma](https://github.com/karma-runner/karma)
+* [eslint](http://eslint.org)
+
+## Requirements
+* node `^4.5.0`
+* yarn `^0.18.0` or npm `^3.0.0`
+
+## Getting Started
---
+++ config/karma.config.js
@@ -0,0 +1,81 @@
+const argv = require('yargs').argv
+const project = require('./project.config')
+const webpackConfig = require('./webpack.config')
+const debug = require('debug')('app:config:karma')
+
+debug('Creating configuration.')
+const karmaConfig = {
+  basePath : '../', // project root in relation to bin/karma.js
+  files    : [
+    {
+      pattern  : `./${project.dir_test}/test-bundler.js`,
+      watched  : false,
+      served   : true,
+      included : true
+    }
+  ],
+  singleRun     : !argv.watch,
+  frameworks    : ['mocha'],
+  reporters     : ['mocha'],
+  plugins: [ 'karma-chrome-launcher', 'karma-mocha',
+    'karma-sourcemap-loader', 'karma-webpack', 'karma-coverage',
+    'karma-mocha-reporter'
+  ],
+  preprocessors : {
+    [`${project.dir_test}/test-bundler.js`] : ['webpack', 'sourcemap']
+  },
+  browsers : ['PhantomJS'],
+  webpack  : {
+    devtool : 'cheap-module-source-map',
+    resolve : Object.assign({}, webpackConfig.resolve, {
+      alias : Object.assign({}, webpackConfig.resolve.alias, {
+        sinon : 'sinon/pkg/sinon.js'
+      })
+    }),
+    plugins : webpackConfig.plugins,
+    module  : {
+      noParse : [
+        /\/sinon\.js/
+      ],
+      rules: webpackConfig.module.rules.concat([
+        {
+          test   : /sinon(\\|\/)pkg(\\|\/)sinon\.js/,
+          loader : 'imports-loader?define=>false,require=>false'
+        }
+      ])
+    },
+        // Enzyme fix, see:
+        // https://github.com/airbnb/enzyme/issues/47
+    externals : Object.assign({}, webpackConfig.externals, {
+      'react/addons'                   : true,
+      'react/lib/ExecutionEnvironment' : true,
+      'react/lib/ReactContext'         : 'window'
+    })// ,
+        // sassLoader : webpackConfig.sassLoader
+  },
+  webpackMiddleware : {
+    noInfo : true
+  },
+  coverageReporter : {
+    reporters : project.coverage_reporters
+  }
+}
+
+if (project.globals.__COVERAGE__) {
+  karmaConfig.reporters.push('coverage')
+    // karmaConfig.webpack.module.preLoaders = [{
+  karmaConfig.webpack.module.rules.push({
+    test    : /\.(js|jsx)$/,
+    enforce: 'pre',
+    include : [
+      new RegExp(project.dir_client)
+    ],
+    exclude : /node_modules/,
+    loader  : 'babel-loader',
+    query   : Object.assign({}, project.compiler_babel, {
+      plugins : (project.compiler_babel.plugins || []).concat('istanbul')
+    })
+  })
+}
+
+module.exports = (cfg) => cfg.set(karmaConfig)
